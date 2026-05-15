0af5650038acfcbced2084096cc3f6ddb0127530
bagoesrex <bagoesrex@gmail.com>
2026-05-16 04:58:43 +0700

    feat: add site constants

---
+++ constants/site.ts
@@ -0,0 +1,11 @@
+interface SiteConfig {
+  name: string;
+  tagline: string;
+  description: string;
+}
+
+export const siteConfig: SiteConfig = {
+  name: "Pixel Wish Box",
+  tagline: "Every pixel holds a dream.",
+  description: "A place to collect, color, and celebrate the wishes that make life worth living.",
+};
