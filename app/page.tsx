9c6db9fd6135687ce074614417f35250da0c5e76
bagoesrex <bagoesrex@gmail.com>
2026-05-16 04:49:04 +0700

    chore: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <p className="text-3xl font-bold">Pixel Wish Box</p>
+    </main>
+  );
+}
